ef162cb8af29eb1f77623a4b8f27c1df1c7eeb5c
Aly <38168385+alycapps@users.noreply.github.com>
2018-09-03 19:03:50 -0400

    added new friend

---
+++ app/data/friends.js
@@ -0,0 +1,17 @@
+var friendsArray = {
+    "name": "Poseidon-Gulliver",
+    "photo": hhh,
+    "scores": [
+        2,
+        1,
+        5,
+        5,
+        4,
+        2,
+        3,
+        4,
+        1,
+        3
+    ]
+}
+
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,11 +2,15 @@ module.exports = function(app, path){
     //route to all friends
     app.get("/api/friends", function(req, res) {
         console.log("all friends page");
-        res.send("howdy friends page")
+        res.send(friendsArray);
     });
 
     //post for incoming survey results
     app.post("/api/friends", function(req, res) {
         console.log("post friends");
+        console.log("you clicked me");
+        var friend = req.body;
+        console.log(friend);
+        res.json(friend);
     });
 };
