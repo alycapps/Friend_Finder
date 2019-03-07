5aa953cb00a88a314418da55d6463b80dff2c743
Aly <38168385+alycapps@users.noreply.github.com>
2019-03-07 18:14:46 -0500

    editing style and cleaning code

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,14 +3,11 @@ var friendsArray = require("../data/friends.js");
 module.exports = function(app, path){
     //route to all friends
     app.get("/api/friends", function(req, res) {
-        console.log("all friends page");
         res.send(friendsArray);
     });
 
     //post for incoming survey results
     app.post("/api/friends", function(req, res) {
-        console.log("post friends");
-        console.log("you clicked me");
         var user = req.body;
         friendsArray.push(user);
     });
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,13 +1,11 @@
 module.exports = function(app, path){
     //home page display
     app.get("/", function(req, res) {
-        console.log("home page");
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 
     //survey route
     app.get("/survey", function(req, res) {
-        console.log("survey page");
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 }
